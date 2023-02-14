a1b437ec7cb774d736aa0c179796f8346ca1b0cd
ivnvch <tarasovets.dmtiv@gmail.com>
2023-02-14 23:30:07 +0300

    add user

---
+++ AutoMarket.Domain/Enum/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoMarket.Domain.Enum
+{
+    public enum Role
+    {
+        [Display(Name ="Пользователь")]
+        User = 0,
+        [Display(Name ="Администратор")]
+        Admin = 1,
+    }
+}
---
+++ AutoMarket.Domain/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Domain.Helpers
+{
+    public static class HashPasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var hashedBytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
