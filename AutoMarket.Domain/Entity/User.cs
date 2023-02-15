2db2a9ee9640e51f3d950fd4dd4a7b675df85fbb
ivnvch <tarasovets.dmtiv@gmail.com>
2023-02-15 22:56:24 +0300

    aut

--- AutoMarket.Domain/Entity/User.cs
+++ AutoMarket.Domain/Entity/User.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMarket.Domain.Enum;
 
 namespace AutoMarket.Domain.Entity
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
     }
 }
--- AutoMarket/Program.cs
+++ AutoMarket/Program.cs
@@ -5,33 +5,28 @@ using AutoMarket.DAL.Interfaces;
 using AutoMarket.DAL.Repositories;
 using AutoMarket.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
 
+//builder.Services.AddControllers();
 builder.Services.AddTransient<IBaseRepository<Car>, CarRepository>();
 builder.Services.AddTransient<ICarService, CarService>();
 builder.Services.AddControllersWithViews();
-//builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-//DefaultFilesOptions options = new DefaultFilesOptions();
-//options.DefaultFileNames.Clear();
-//options.DefaultFileNames.Add("index.html");
-//app.UseDefaultFiles(options);
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseStaticFiles();
-
 
 app.MapControllerRoute(
     name: "default",
@@ -40,5 +35,4 @@ app.MapControllerRoute(
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.Run();
